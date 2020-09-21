069c3bfe258ae178425989fd94664342c9a7fa7f
byron <liangzhaocong@163.com>
2020-09-21 15:53:42 +0800

    feat: add docker sample file
    1. add sample etcd with docker config file

--- docker/docker-file/enhance/base/balance/main.go
+++ docker/docker-file/enhance/base/balance/main.go
@@ -3,17 +3,24 @@ package main
 import (
     "fmt"
     "balance/app"
+    "flag"
 )
 
-var factory *app.BalanceFactory
+var (
+    factory *app.BalanceFactory
+    port, len int
+)
 
 func init()  {
     factory = app.NewBalanceFactory()
     factory.AddContainer("robin", app.NewRoundRobinBalance(0))
+    flag.IntVar(&port, "port", 8080, "set port")
+    flag.IntVar(&len, "len", 4, "set init data length")
 }
 
 func main()  {
-    data := app.InitBalanceData(4, 8080)
+    flag.Parse()
+    data := app.InitBalanceData(len, port)
     for _, res := range data {
         fmt.Println("origin: ", res.String())
     }
